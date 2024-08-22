12e2f613dac950585639c862b590267dac1afa10
yechan0426 <kwonyechan0426@gmail.com>
2024-08-22 16:32:50 +0900

    1

---
+++ Huffman/Huffman.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <cstdio>
+
+using namespace std;
+
+struct Node {
+    char c = '\0';  //문자열
+    int cnt;        //빈도수
+    Node* left;     //왼쪽 노드
+    Node* right;    //오른쪽 노드
+};
+Node* root_node;    //루트 노드
+
+string s_input;                      //입력된 문자열 (string형)
+vector<bool> compressed;             //압축된 문자열 (bool형)
+unordered_map<char, int> frequency;  //문자의 빈도수
+
+int main()
+{
+    int menu_num;
+    string input_file_name, output_file_name;
+    FILE* fin = nullptr;
+    FILE* fout = nullptr;
+
+    while (true) {
+        cout << "1: 압축, 2: 압축 해제, 3: 종료\n";
+        cin >> menu_num;
+        if (menu_num == 1) {
+            cout << "입력할 파일명 입력: ";
+            cin >> input_file_name;
+            cout << "출력할 파일명 입력: ";
+            cin >> output_file_name;
+            fin = fopen(input_file_name.c_str(), "rt");
+            fout = fopen(output_file_name.c_str(), "")
+        }
+    }
+}
+/*
+https://blockdmask.tistory.com/392
+*/
---
+++ LZ77/LZ77.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+#define LOOKAHEAD_BUFF_SIZE 64
+#define WINDOW_BUFF_SIZE    32
+
+using namespace std;
+
+int main()
+{
+	string INPUT, OUTPUT, tmp;
+	int literal, length;
+	char distance;
+	int cnt;
+
+	while (true) {
+		getline(cin, tmp);
+		if (tmp == "!STOP") { break; }
+		INPUT += tmp;
+	}
+
+	OUTPUT = "LB";
+	OUTPUT += to_string(LOOKAHEAD_BUFF_SIZE);
+	OUTPUT += "WB";
+	OUTPUT += to_string(WINDOW_BUFF_SIZE);
+
+	int i, k;
+	for (i = 0; i < INPUT.size(); i++) {
+		literal = 0;
+		length = 0;
+		k = i - LOOKAHEAD_BUFF_SIZE;
+		distance = INPUT[i];
+		if (k < 0) {
+			k = 0;
+		}
+		for (k; k < i; k++) {
+			cnt = 0;
+			while (i + cnt + 1 < INPUT.size() && k < i && INPUT[k] == INPUT[i + cnt]) {
+				cnt++;
+				k++;
+			}if (cnt == 0) {
+				continue;
+			}
+			if (cnt >= length) {
+				length = cnt;
+				literal = i - k + cnt;
+				distance = INPUT[i + cnt];
+			}
+		}
+		i += length;
+		OUTPUT += "(";
+		OUTPUT += to_string(literal);
+		OUTPUT += ",";
+		OUTPUT += to_string(length);
+		OUTPUT += ",";
+		OUTPUT += distance;
+		OUTPUT += ")";
+	}
+
+	cout << OUTPUT;
+
+	return 0;
+}
